a3472fbbd62ccfaee57affafa5cc9757150fcfc5
Jason Sato <jsato03@gmail.com>
2019-05-25 10:39:15 -0700

    Add implementation for resistor-color (python)

---
+++ python/resistor-color/resistor_color.py
@@ -0,0 +1,19 @@
+colorCodeDict = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9
+}
+
+def color_code(color):
+    return colorCodeDict.get(color)
+
+
+def colors():
+    return list(colorCodeDict.keys())

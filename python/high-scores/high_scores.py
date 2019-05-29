36e8df69fe9997325da23ad0852e8c184f0f6d09
Jason Sato <jsato03@gmail.com>
2019-05-29 08:01:39 -0700

    Refactor functions for high-scores

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -1,13 +1,8 @@
 def latest(scores):
-    return scores[len(scores)-1]
+    return scores[-1]
 
 def personal_best(scores):
-    best = 0
-    for val in scores:
-        if val > best:
-            best = val
-    return best
+    return max(scores)
 
 def personal_top_three(scores):
-    scores.sort(reverse=True)
-    return scores[0:3]
+    return sorted(scores, reverse=True)[:3]

e533acb82bad0ba8a120ad9a6a0701683adb2ea1
Jason Sato <jsato03@gmail.com>
2019-06-21 09:54:27 -0700

    Add implementation and tests for hamming.py

---
+++ python/hamming/hamming.py
@@ -0,0 +1,8 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be same length!")
+    hammingDistance = 0
+    for i in range(len(strand_a)):
+        if strand_a[i] != strand_b[i]:
+            hammingDistance += 1
+    return hammingDistance

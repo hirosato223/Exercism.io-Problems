652b9a03badd864f210b704dea680bed52465fc8
Jason Sato <jsato03@gmail.com>
2019-06-21 10:01:17 -0700

    Commit implementation for collatz_conjecture.py

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
 def steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Input value must be a positive whole number")
+    steps = 0
+    while number > 1:
+        if number % 2 == 0:
+            number = number / 2
+        elif number % 2 == 1:
+            number = number * 3 + 1
+        steps += 1
+    return steps
